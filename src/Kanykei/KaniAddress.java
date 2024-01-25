d8ab6cae231175466481084c3f8963f5a0ab1591
TestMain <momuntaevakanykei@gmail.com>
2024-01-25 20:38:07 +0600

    My first commit

---
+++ src/Kanykei/KaniAddress.java
@@ -0,0 +1,4 @@
+package Kanykei;
+
+public class KaniAddress {
+}
---
+++ src/Kanykei/KaniEmail.java
@@ -0,0 +1,8 @@
+package Kanykei;
+
+public class KaniEmail {
+    public static void main(String[] args) {
+
+        System.out.println("Enter your email: ");
+    }
+}
